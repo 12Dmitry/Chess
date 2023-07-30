482981aab0e5d8587bdeea5beba1a0294181f8cb
Dmitry <Dmitry@HEROBOOK-PRO>
2023-07-30 14:50:14 +0400

    пробую рефлиховать проверку с enum

--- Chessman.cs
+++ Chessman.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace Chess
 {
-    public enum Chessman { Pawn = 1, Horse = 3, Bishop = 3, Rook = 5, Queen = 9, King = 7 }; //TODO: на юлеарн показывали как и где его ставить.
+    public enum Chessman { Nun = 0, Pawn = 1, Horse = 3, Bishop = 3, Rook = 5, Queen = 9, King = 7 }; //TODO: на юлеарн показывали как и где его ставить.
 
     public abstract class ClassChessman
     {
+        public Chessman Chessman { get; set; }
         public bool IsWhite { get; set; }
         //public Imagine imagine { get; set; }
 
@@ -18,6 +19,7 @@ namespace Chess
 
     public class Pawn : ClassChessman
     {
+        Chessman chesman = Chessman.Pawn;
         public override bool CheckMove(Point initial, Point final)
         {
             if (initial.GetX != final.GetX || initial.GetY + 1 != final.GetY)
@@ -28,11 +30,13 @@ namespace Chess
 
     public class Horse : ClassChessman
     {
+        Chessman chesman = Chessman.Horse;
         public override bool CheckMove(Point initial, Point final)
         {
             if ((Math.Abs(initial.GetX - final.GetX) != 1 || Math.Abs(initial.GetY - final.GetY) != 2)
             && (Math.Abs(initial.GetX - final.GetX) != 2 || Math.Abs(initial.GetY - final.GetY) != 1))
                 return false;
+            if
             else return true;
         }
     }
